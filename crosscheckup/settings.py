80c7d1f6e0761f245e12b54b7ab9c86c7578dd29
karthik <cka4523@gmail.com>
2026-07-11 00:17:16 +0530

    Add smtp

--- crosscheckup/settings.py
+++ crosscheckup/settings.py
@@ -150,6 +150,9 @@ REST_FRAMEWORK = {
     ),
 }
 
+BREVO_API_KEY = config('BREVO_API_KEY', default='')
+BREVO_SENDER_EMAIL = config('BREVO_SENDER_EMAIL', default='')
+BREVO_SENDER_NAME = config('BREVO_SENDER_NAME', default='Enterprise Auth')
 # --------------------------------------------------------------------------
 # Email (Gmail SMTP)
 # --------------------------------------------------------------------------
